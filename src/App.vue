eabacde5f25cc32d20d4bafca1eb0544fc1d6850
Serkan Algur <info@wpadami.com>
2024-06-22 15:52:54 +0400

    First Commit and test button. I will add more functionality to reuse in my projects

---
+++ src/App.vue
@@ -0,0 +1,10 @@
+<script setup>
+  import PersonalButton from "./components/Button.vue";
+</script>
+
+<template>
+  <div>
+    <p>Test Personal Button</p>
+    <PersonalButton text="Test Button" />
+  </div>
+</template>
---
+++ src/components/Button.vue
@@ -0,0 +1,24 @@
+<template>
+  <button class="personal-button" @click="showAlert">
+    {{ props.text }}
+  </button>
+</template>
+
+<script setup>
+  const props = defineProps(["text"]);
+
+  function showAlert() {
+    console.log("alert");
+  }
+</script>
+
+<style scoped>
+  .personal-button {
+    background-color: #000;
+    color: #fff;
+    padding: 10px 20px;
+    border: none;
+    border-radius: 5px;
+    cursor: pointer;
+  }
+</style>
